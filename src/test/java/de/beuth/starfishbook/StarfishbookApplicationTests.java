84dafbeb0246a531f857fdd92f20d227fec9f833
Büsra Kumru <kkumrubusra@gmail.com>
2022-05-05 17:00:08 +0200

    first commit

---
+++ src/test/java/de/beuth/starfishbook/StarfishbookApplicationTests.java
@@ -0,0 +1,13 @@
+package de.beuth.starfishbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarfishbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
