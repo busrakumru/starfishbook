34f45f5091a976226490a97212c2a02222d84cdf
Gonacgül Demir <s33616@bht-berlin.de>
2022-05-20 21:00:10 +0200

    kleine Änderungen

--- src/main/java/de/beuth/starfishbook/controller/NotesController.java
+++ src/main/java/de/beuth/starfishbook/controller/NotesController.java
@@ -2,11 +2,10 @@ package de.beuth.starfishbook.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/de/beuth/starfishbook/exception/NoteNotFoundException.java
+++ src/main/java/de/beuth/starfishbook/exception/NoteNotFoundException.java
@@ -2,7 +2,7 @@ package de.beuth.starfishbook.exception;
 
 public class NoteNotFoundException extends Exception {
 
-    private long id;
+
     
     public NoteNotFoundException(long id) {
     super(String.format("Note is not found with id : '%s'", id));
