5386b8d4929b08235112babf3a18b51f9281c586
Gonacgül Demir <s33616@bht-berlin.de>
2022-07-16 00:21:35 +0200

    todo frontend finish

--- src/main/resources/frontend/starfishbook/src/app/modals/todo/todo.module.ts
+++ src/main/resources/frontend/starfishbook/src/app/modals/todo/todo.module.ts
@@ -5,14 +5,19 @@ import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { TodoPageRoutingModule } from './todo-routing.module';
-
+import {MatIconModule} from '@angular/material/icon';
 import { TodoPage } from './todo.page';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
     TodoPageRoutingModule,
     ReactiveFormsModule
   ],
