2d2584e25b4993fc290151766e7a45727c879422
Gonacgül Demir <s33616@bht-berlin.de>
2022-07-20 22:19:19 +0200

    setting page with dark mode function

--- src/main/resources/frontend/starfishbook/src/app/app-routing.module.ts
+++ src/main/resources/frontend/starfishbook/src/app/app-routing.module.ts
@@ -27,7 +27,11 @@ const routes: Routes = [
   {
     path: 'todo',
     loadChildren: () => import('./modals/todo/todo.module').then( m => m.TodoPageModule)
+  },  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
   }
+
 
 ];
 @NgModule({
--- src/main/resources/frontend/starfishbook/src/app/tab4/tab4.page.ts
+++ src/main/resources/frontend/starfishbook/src/app/tab4/tab4.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { SettingsPage } from '../settings/settings.page';
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  async openModal() {
+
+    const modal = await this.modalController.create({
+      component: SettingsPage,
+    });
+    return await modal.present();
   }
 
 }
